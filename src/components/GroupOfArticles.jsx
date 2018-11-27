a7ec3b9a4b14ae51689882d01a2f57c93cc6d082
dpocknee <dappy_iodo@hotmail.com>
2018-11-27 13:03:32 +0000

    putting in loading information.

--- src/components/GroupOfArticles.jsx
+++ src/components/GroupOfArticles.jsx
@@ -6,30 +6,36 @@ import { isEqual } from 'lodash';
 
 class GroupOfArticles extends Component {
   state = {
-    articles: []
+    articles: [],
+    isLoading: true
   };
   render() {
     return (
       <div>
         <header>
-          <h1>Articles</h1>
+          <h1>
+            {this.props.topic_slug && `${capitalizer(this.props.topic_slug)}`}{' '}
+            Articles
+          </h1>
         </header>
-        {this.state.articles.map((article, index) => (
-          <Article key={`article${index}`} articleInfo={article} />
-        ))}
+        {this.state.isLoading && <p>... loading articles ...</p>}
+        {!this.state.isLoading &&
+          this.state.articles.map((article, index) => (
+            <Article key={`article${index}`} articleInfo={article} />
+          ))}
       </div>
     );
   }
   componentDidMount() {
     if (this.props.topic_slug) {
       api.getArticlesByTopic(this.props.topic_slug).then(articles => {
-        this.setState({ articles });
+        this.setState({ articles, isLoading: false });
       });
     } else {
       api
         .getInfo('articles')
         .then(articles => {
-          this.setState({ articles });
+          this.setState({ articles, isLoading: false });
         })
         .catch(console.log);
     }
@@ -37,23 +43,32 @@ class GroupOfArticles extends Component {
 
   componentDidUpdate(prevProps, prevState) {
     if (this.props.topic_slug) {
-      api.getArticlesByTopic(this.props.topic_slug).then(articles => {
-        if (!isEqual(prevState.articles, articles)) {
-          this.setState({ articles });
-        }
-      });
+      // this.setState({ isLoading: true });
+      api
+        .getArticlesByTopic(this.props.topic_slug)
+        .then(articles => {
+          if (!isEqual(prevState.articles, articles)) {
+            this.setState({ articles, isLoading: false });
+          }
+        })
+        .catch(console.log);
     } else {
+      // this.setState({ isLoading: true });
       api
         .getInfo('articles')
         .then(articles => {
           if (!isEqual(prevState.articles, articles)) {
-            this.setState({ articles });
+            this.setState({ articles, isLoading: false });
           }
         })
         .catch(console.log);
     }
   }
 }
+
+const capitalizer = word => {
+  return word[0].toUpperCase() + word.slice(1);
+};
 
 GroupOfArticles.propTypes = {};
 
