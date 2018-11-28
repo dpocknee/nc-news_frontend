7ec38d87276f7d40592ad5abc9360e13194ed003
dpocknee <dappy_iodo@hotmail.com>
2018-11-28 17:28:24 +0000

    halfway through implementing POST request for articles.

--- src/api.js
+++ src/api.js
@@ -11,3 +11,8 @@ export const changeVotes = async (url, upOrDown) => {
   const { data } = await axios.patch(`${BASE_URL}/${url}?vote=${upOrDown}`);
   return data;
 };
+
+export const addInfo = async (url, body) => {
+  const { data } = await axios.post(`${BASE_URL}/${url}`, body);
+  return data;
+};
--- src/components/AddArticle.jsx
+++ src/components/AddArticle.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import '../css/AddArticle.css';
+import * as api from '../api';
 
 class AddArticle extends Component {
+  state = {
+    title: '',
+    textarea: ''
+  };
   render() {
     return (
-      <div>
+      <div className="wholeAdder">
         {this.props.topic_slug && (
           <div className="addarticle">
             <div className="plus">
@@ -16,9 +21,50 @@ class AddArticle extends Component {
             </div>
           </div>
         )}
+        <form className="addArticleForm">
+          <h3>Topic: {this.props.topic_slug}</h3>
+          <label htmlFor="titleInput">Title:</label>
+          <input
+            type="text"
+            name="titleInput"
+            id="titleInput"
+            onChange={event => this.handleInput(event, 'title')}
+          />
+          <br />
+          <label htmlFor="textAreaInput">Article:</label>
+          <textarea
+            id="textAreaInput"
+            rows="20"
+            cols="100"
+            onChange={event => this.handleInput(event, 'textarea')}
+          />
+          <br />
+          <button type="submit" onClick={this.handleSubmit}>
+            Post
+          </button>
+        </form>
       </div>
     );
   }
+  handleInput = (event, type) => {
+    const eventValue = event.target.value;
+    this.setState({ [type]: eventValue });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const body = {
+      title: this.state.title,
+      body: this.state.textarea,
+      created_by: localStorage.getItem('ncuser')
+    };
+    console.log('working??', body);
+    console.log('url', `topics/${this.props.topic_slug}/articles`);
+    api
+      .addInfo(`topics/${this.props.topic_slug}/articles`, body)
+      .then(res => console.log('post response', res))
+      .catch(console.log);
+  };
 }
 
 AddArticle.propTypes = {};
